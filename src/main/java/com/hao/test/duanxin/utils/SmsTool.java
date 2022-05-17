6b0a92dd71c8d99aa2e61f3b7da46ea0804cc409
Maybe-Hao <83172459+Maybe-Hao@users.noreply.github.com>
2022-05-17 21:09:50 +0800

    Update SmsTool.java

--- src/main/java/com/hao/test/duanxin/utils/SmsTool.java
+++ src/main/java/com/hao/test/duanxin/utils/SmsTool.java
@@ -15,8 +15,8 @@ public class SmsTool {
     //产品域名,开发者无需替换
     static final String domain = "dysmsapi.aliyuncs.com";
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "LTAI5tHXL76kgu6YwmH5EnrE";
-    static final String accessKeySecret = "LAZv95VkFPaBFHYpMhHHeZZp0rbqbK";
+    static final String accessKeyId = "";
+    static final String accessKeySecret = "";
 
     public static SendSmsResponse sendSms(String phone , String code) throws ClientException {
         //可自助调整超时时间
